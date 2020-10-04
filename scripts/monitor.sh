81ddc9b3d745afee5298f6fc38ebc8220c9397e5
JMS55 <47158642+JMS55@users.noreply.github.com>
2020-10-04 17:29:18 -0400

    Revamp monitor.sh and wait.sh

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-container_id=$1
-
+# Reads lines of MITM logs from STDIN
 while read line; do
     attacker_ip=$(echo $line | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
 
-    echo "New login: $attacker_ip, $(date)" >> /root/${container_id}log
-
+    # ACCEPT traffic from { $attacker_ip } to { container }
     iptables --table filter --insert INPUT 1 --protocol tcp --source $attacker_ip --destination 172.20.0.1 --destination-port 10000 --jump ACCEPT
+    # DROP traffic from { anywhere } to { container }
     iptables --table filter --insert INPUT 2 --protocol tcp --source 0.0.0.0/0 --destination 172.20.0.1 --destination-port 10000 --jump DROP
 
-    ./wait.sh $container_id $attacker_ip &
-    exit
+    # Goto wait.sh
+    ./wait.sh $attacker_ip &
+    break
 done
--- scripts/wait.sh
+++ scripts/wait.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-container_id=$1
-attacker_ip=$2
+attacker_ip=$1
 
+# Wait 45m for the attacker to do things
 sleep 45m
 
+# Remove firewall rules set by monitor.sh
 iptables --table filter --delete INPUT --protocol tcp --source $attacker_ip --destination 172.20.0.1 --destination-port 10000 --jump ACCEPT
 iptables --table filter --delete INPUT --protocol tcp --source 0.0.0.0/0 --destination 172.20.0.1 --destination-port 10000 --jump DROP
 
-if [[ -f /var/lib/lxc/${container_id}/rootfs/root/malware.txt ]]; then
-    iptables --table filter --insert INPUT 1 --source $attacker_ip --destination 172.20.0.1 --jump DROP
-fi
+# DROP traffic from { $attacker_ip } to { container } forever
+iptables --table filter --insert INPUT 1 --source $attacker_ip --destination 172.20.0.1 --jump DROP
 
+# Goto recyle.sh
 ./recycle.sh $container_id &
