5124a517c787db8cf3a40fb7727c63289dfebe87
JMS55 <47158642+JMS55@users.noreply.github.com>
2020-10-27 16:54:26 -0400

    Dont overwrite logs, try to fix MITM

--- configs/mitm_config.js
+++ configs/mitm_config.js
@@ -39,7 +39,7 @@ module.exports = {
             },
             fixed: {
                 enabled: true,
-                attempts: 1,
+                attempts: 2,
             }
         }
     }
