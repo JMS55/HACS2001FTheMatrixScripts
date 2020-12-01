e72b71ee8a4ba0e8c5d0c2dfabf385263328c23e
Gudrun-Thorkelsdottir <gudrun2001@gmail.com>
2020-12-01 17:27:13 -0500

    Update duration.py

--- scripts/data_analysis/duration.py
+++ scripts/data_analysis/duration.py
@@ -28,27 +28,3 @@ for Attack in attacks:
 	
 	
 out.close()
-
-#files = [file1, file2, file3, file4]
-#
-#for file in files:
-#
-#	Lines = file.readlines()
-#
-#	for i in range(len(Lines)):
-#		
-#		line = Lines[i]
-#		if "Attacker connected" in line:
-#			start_time = line.split()[1]
-#
-#			for j in range(len(Lines) - i):
-#				end_line = Lines[j + i]
-#				if "Attacker closed the connection" in end_line:
-#					end_time = end_line.split()[1]
-#					out.write(end_time - start_time)
-#					break
-#
-#
-#	file.close()
-#
-#out.close()		
