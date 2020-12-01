6c31e64ea637d8c501707bbd1dfb620835a3f0f8
Gudrun-Thorkelsdottir <gudrun2001@gmail.com>
2020-12-01 17:21:08 -0500

    Update duration.py

--- scripts/data_analysis/duration.py
+++ scripts/data_analysis/duration.py
@@ -5,20 +5,26 @@ out = open("durations.txt", "a")
 
 attacks = parser.getAllAttacks(".")
 
+date_format = "%Y-%m-%d %H:%M:%S"
+
 for Attack in attacks:
 	
 	Lines = Attack.data.splitlines()
-	start_time, end_time
+	start_time = ""
+	end_time = ""
 	
 	for line in Lines:
 		
 		if "Attacker connected" in line:
-			start_time = line.split()[1]
+			start_time = line.split()[0] + " " + (line.split()[1]).split(".")[0]
 			
-		if "Attacker closed the connection" in end_line:
-					end_time = end_line.split()[1]
+		if "Attacker closed the connection" in line:
+			end_time = line.split()[0] + " " + (line.split()[1]).split(".")[0]
 				
-	out.write(end_time - start_time)
+	if start_time != "" && end_time != "":
+		end = datetime.strptime(end_time, date_format)
+		start = datetime.strptime(start_time, date_format)
+		out.write((end - start).seconds)
 	
 	
 out.close()
