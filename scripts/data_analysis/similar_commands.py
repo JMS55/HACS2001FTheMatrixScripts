84b27c1da7d17db199deb632ad2336bfceec801f
JMS55 <47158642+JMS55@users.noreply.github.com>
2020-12-06 16:33:23 -0500

    Remove duplicate results, I'm dumb

--- scripts/data_analysis/similar_commands.py
+++ scripts/data_analysis/similar_commands.py
@@ -5,36 +5,32 @@ import pprint
 reg = re.compile(r'\n|\|\||;')
 pp = pprint.PrettyPrinter()
 
-def calc(with_cat):
-    results_control = {}
-    results_experimental = {}
+results_control = {}
+results_experimental = {}
 
-    attacks = parser.getAllAttacks(".", with_cat)
-    for attack in attacks:
-        commands = reg.split(attack.noninteractiveCommand)
-        while ("" in commands):
-            commands.remove("")
+attacks = parser.getAllAttacks(".", True)
+for attack in attacks:
+    commands = reg.split(attack.noninteractiveCommand)
+    while ("" in commands):
+        commands.remove("")
 
-        for command in commands:
-            if attack.template == "201":
-                if command in results_control:
-                    results_control[command] += 1
-                else:
-                    results_control[command] = 1
+    for command in commands:
+        if attack.template == "201":
+            if command in results_control:
+                results_control[command] += 1
             else:
-                if command in results_experimental:
-                    results_experimental[command] += 1
-                else:
-                    results_experimental[command] = 1
+                results_control[command] = 1
+        else:
+            if command in results_experimental:
+                results_experimental[command] += 1
+            else:
+                results_experimental[command] = 1
 
-    print("With Cat?: " + str(with_cat))
-    pp.pprint("Control Results:")
-    pp.pprint(results_control)
-    print("")
-    pp.pprint("Experimental Results:")
-    pp.pprint(results_experimental)
-    print("")
+results_control = sorted(results_control.items(), key=lambda x: x[1], reverse=True)
+results_experimental = sorted(results_experimental.items(), key=lambda x: x[1], reverse=True)
 
-calc(True)
+pp.pprint("Control Results:")
+pp.pprint(results_control)
 print("")
-calc(False)
+pp.pprint("Experimental Results:")
+pp.pprint(results_experimental)
